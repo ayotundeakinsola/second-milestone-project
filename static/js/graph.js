c1473c9c015b3f05d15471f11e63cd86441176d1
Ubuntu <ubuntu@ip-172-31-93-50.ec2.internal>
2019-07-30 09:31:17 +0000

    Added another barchart to index.html & graph.js

--- static/js/graph.js
+++ static/js/graph.js
@@ -5,8 +5,13 @@ queue()
 function makeGraphs(error, imdbData) {
     var ndx = crossfilter(imdbData);
 
+    imdbData.forEach(function(d) {
+        d.Title = parseInt(d.Title);
+    })
+
     show_rating(ndx);
-    
+    show_average_runtime(ndx);
+
     dc.renderAll();
 }
 
@@ -23,8 +28,55 @@ function show_rating(ndx) {
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .elasticY(true)
         .xAxisLabel("Movie")
         .yAxisLabel("Rating")
-        .yAxis().ticks(10);
+        .yAxis().ticks(15);
+}
+
+function show_average_runtime(ndx) {
+    var dim = ndx.dimension(dc.pluck('Runtime'));
+
+    function add_item(p, v) {
+        p.count++;
+        p.total += v.Title;
+        p.average = p.total / p.count;
+        return p;
+    }
+
+    function remove_item(p, v) {
+        p.count--;
+        if (p.count == 0) {
+            p.total = 0;
+            p.average = 0;
+        }
+        else {
+            p.total -= v.Title;
+            p.average = p.total / p.count;
+        }
+        return p;
+    }
+
+    function initialise() {
+        return { count: 0, total: 0, average: 0 };
+    }
+
+    var averageRuntimeofMovie = dim.group().reduce(add_item, remove_item, initialise);
+
+    dc.barChart("#average-runtime")
+        .width(900)
+        .height(300)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(averageRuntimeofMovie)
+        .valueAccessor(function(d) {
+            return d.value.average.toFixed(2);
+        })
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Title")
+        .yAxisLabel("Runtime")
+        .yAxis().ticks(15);
+
 }

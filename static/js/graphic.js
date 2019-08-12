7a72832a9540bf8cfd8306e29c6c270b3a89d791
Ubuntu <ubuntu@ip-172-31-93-50.ec2.internal>
2019-08-12 01:33:31 +0000

    created a pie chart

--- static/js/graphic.js
+++ static/js/graphic.js
@@ -4,24 +4,25 @@ queue()
 
 function makeGraphs(error, imdbsData) {
     var ndx = crossfilter(imdbsData);
-    
-    imdbsData.forEach(function(d){
+
+    imdbsData.forEach(function(d) {
         d.Revenue = parseInt(d.Revenue);
     })
 
     show_genre_selector(ndx);
     show_movie_year(ndx);
+    show_ratings_genre(ndx);
 
     dc.renderAll();
 }
 
 
 //Genre Selector
-function show_genre_selector(ndx){
-     var dim = ndx.dimension(dc.pluck('Genre'));
-     var group = dim.group();
-     
-      dc.selectMenu("#genre-selector")
+function show_genre_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('Genre'));
+    var group = dim.group();
+
+    dc.selectMenu("#genre-selector")
         .dimension(dim)
         .group(group);
 }
@@ -33,10 +34,10 @@ function show_movie_year(ndx) {
     var dim = ndx.dimension(dc.pluck('Year'));
     var group = dim.group();
 
-     dc.barChart("#movie-year")
+    dc.barChart("#movie-year")
         .width(800)
         .height(300)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(group)
         .transitionDuration(500)
@@ -44,8 +45,20 @@ function show_movie_year(ndx) {
         .xUnits(dc.units.ordinal)
         .elasticY(true)
         .xAxisLabel("Year")
-        .yAxis().ticks(6);   
-        
+        .yAxis().ticks(6);
+
 }
 
 
+//Pie Chart
+function show_ratings_genre(ndx) {
+    var rating_dim = ndx.dimension(dc.pluck('Genre'));
+    var total_rating_per_genre = rating_dim.group().reduceSum(dc.pluck('Rating'));
+    
+    dc.pieChart('#rating')
+        .height(330)
+        .radius(90)
+        .transitionDuration(1500)
+        .dimension(rating_dim)
+        .group(total_rating_per_genre);
+}
